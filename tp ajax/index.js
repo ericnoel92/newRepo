e97c0a1d3f7050e186b2fd12bfc6e1cd339ffc55
Eric <eric.antonie.noel92@gmail.com>
2024-03-08 09:27:45 +0100

    tp ajax

---
+++ tp ajax/index.js
@@ -0,0 +1,10 @@
+console.log('bonjour');
+document.addEventListener("DOMContentLoaded", function () {
+    
+    function convertCurrency() {
+        
+    }
+
+    
+    document.getElementById("convertBtn").addEventListener("click", convertCurrency);
+});
